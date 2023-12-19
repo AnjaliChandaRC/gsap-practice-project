52d27519172264b0f7d9bab2dc3fbc9c3217a595
sukhvinder-jaat <monudhaka829@gmail.com>
2023-12-19 09:51:32 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Main from "./views/Main";
+import Main2 from "./views/Main2";
 
 function App() {
   return (
     <>
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/main2" element={<Main2 />} />
+      </Routes>
     </>
   );
 }
