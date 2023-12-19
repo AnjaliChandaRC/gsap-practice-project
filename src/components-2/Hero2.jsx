9c2645255c2f7b56c5e872efaa4926309e100994
sukhvinder-jaat <monudhaka829@gmail.com>
2023-12-19 10:15:44 +0530

    icons added

--- src/components-2/Hero2.jsx
+++ src/components-2/Hero2.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React, { useLayoutEffect } from "react";
+import { HeroIcon, HeroIconSecond } from "../common/Icons";
 
 const Hero2 = () => {
-  return <div>Hero2</div>;
+  // gsap.registerPlugin(ScrollTrigger);
+  ScrollTrigger.defaults({
+    toggleActions: "play complete none reverse",
+  });
+  useLayoutEffect(() => {
+    let icons = gsap.matchMedia();
+    icons.add("(max-width:767.98px)", () => {
+      let revenue = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".iconsbar_parent",
+          start: "top 30%",
+          end: "bottom center",
+          scrub: 3,
+          pin: true,
+        },
+      });
+      revenue.fromTo(
+        ".icons_img",
+        {
+          xPercent: 0,
+        },
+        {
+          xPercent: -120,
+        }
+      );
+      // },
+    });
+  }, []);
+  return (
+    <div>
+      <div class="home_title__U0Uwb bg-black">
+        <HeroIcon />
+        <HeroIconSecond />
+      </div>
+    </div>
+  );
 };
 
 export default Hero2;
